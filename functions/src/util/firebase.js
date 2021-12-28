ff4b3cbe46c0936d4108bb2ca30226ccb5ac3f63
HongJinHyeok <redmax45@naver.com>
2021-12-29 00:12:54 +0900

    firebase init

--- functions/src/util/firebase.js
+++ functions/src/util/firebase.js
@@ -1,10 +1,25 @@
 'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+
+exports.firebase = exports.db = exports.auth = void 0;
 
 const firebase = require('firebase-admin');
 const serviceAccount = require('../config/adminsdk.json');
-firebase.initializeApp({
-  credential: firebase.credential.cert(serviceAccount),
-  databaseURL: 'https://test-9f6df.firebaseio.com',
-});
 
-export default firebase;
+let isInitializeApp = false;
+if (!isInitializeApp) {
+  firebase.initializeApp({
+    credential: firebase.credential.cert(serviceAccount),
+    databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`,
+  });
+  isInitializeApp = true;
+}
+
+const auth = firebase.auth();
+exports.auth = auth;
+
+const db = firebase.firestore();
+exports.db = db;
+
+const settings = { timestampsInSnapshots: true };
+db.settings(settings);
